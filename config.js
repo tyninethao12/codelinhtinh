238726ded39bb755c699049a4268457abf5aca5f
nguyenvietcode <101140152+nguyenvietcode@users.noreply.github.com>
2022-03-08 14:27:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Chuc Mưng 8-3 <3",
-    introTitle: 'Nguyen Ngoc Mai',
+    introTitle: 'C Thẻooo',
     introDesc: `Ngày 8/3 đã đến rồi, chúc một nửa xinh đẹp của thế giới luôn thành công và hạnh phúc trong cuộc sống. Chúc cô bạn của tôi luôn xinh đẹp, duyên dáng và đáng yêu trong mắt một nửa thế giới còn lại.`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, em qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/VietAnh.Dvfb.Info' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
